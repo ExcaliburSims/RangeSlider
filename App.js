e478bc5f89d423b815d0a925350ab0cc7b99873e
ExcaliburSims <ximeonzilu@gmail.com>
2023-02-08 13:11:51 +0100

    feat: add API link

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Card from "./components/card";
 
 export default function App() {
+  const API = "https://dummyjson.com/users?limit=20";
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(
+          "console :" + JSON.stringify(responseJson.users[0].maidenName),
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>FILTER</Text>
+      <Text style={styles.texte}>FILTER</Text>
       <Card />
       <StatusBar style="auto" />
     </View>
@@ -16,7 +30,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 30,
+    /*  alignItems: "center",
+     justifyContent: "center",*/
+  },
+  texte: {
+    marginTop: 10,
+    textAlign: "center",
   },
 });
